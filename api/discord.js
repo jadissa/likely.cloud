ad7eb52f8cea4633a93023f2f7a94cddbb544020
Jadissa Griffin <jadissag@gmail.com>
2017-10-21 22:53:56 -0700

    Found another one

--- api/discord.js
+++ api/discord.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const redirect = encodeURIComponent('http://localhost:50451/api/discord/callback');
+const redirect = encodeURIComponent('http://urlike.me:50451/api/discord/callback');
 
 router.get('/login', (req, res) => {
   res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
