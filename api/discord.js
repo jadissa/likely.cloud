c48d4c577e1692cb2555444002ac34eeb23a00fc
Jadissa Griffin <jadissag@gmail.com>
2017-10-24 21:42:18 -0700

    omg this...thing

--- api/discord.js
+++ api/discord.js
@@ -120,10 +120,10 @@ router.get('/callback', catchAsync(async (req, res) => {
   //
   //  Process the invite
   //
-  const join_response = await fetch(`https://discordapp.com/api/invites/${invite_json.id}`,
+  const join_response = await fetch(`https://discordapp.com/api/invites/${INVITE_CODE}`,
   {
 
-    method: 'GET',
+    method: 'POST',
 
       headers: {
 
