c0440d8b2571b72f83f501478639069aa21bba3f
Jadissa Griffin <jadissag@gmail.com>
2017-10-24 22:11:25 -0700

    All is done minus the redirect to our homepage

--- api/discord.js
+++ api/discord.js
@@ -142,9 +142,7 @@ router.get('/callback', catchAsync(async (req, res) => {
   //
   //  Redirect user to homepage
   //
-  response.writeHead(302,  {Location: `http://likely.cloud/`})
-
-  response.end();
+  res.redirect('/');
 
 }));
 
