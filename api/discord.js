c49bd550e95379f0ca4cc8b383347d264e18d8c5
Jadissa Griffin <jadissag@gmail.com>
2017-10-24 21:19:48 -0700

    Added fetching for invite object

--- api/discord.js
+++ api/discord.js
@@ -6,6 +6,17 @@ const btoa = require('btoa');
 
 const util = require('util');
 
+//
+//  The discord consumer
+//  Authenticates a user and logs them into our Discord channel
+//  During this process, we capture their Discord registration `email` and `username`
+//  extract this information and use it internally for our own registration
+//
+//  @todo: It's going to eventually be necessary to handle negative response codes
+//  eg. 429 (TOO MANY REQUESTS)
+//  Reference: https://discordapp.com/developers/docs/topics/response-codes
+//
+
 const { catchAsync } = require('../utils');
 
 const router = express.Router();
@@ -13,6 +24,8 @@ const router = express.Router();
 const CLIENT_ID = process.env.CLIENT_ID;
 
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+const INVITE_CODE = process.env.INVITE_CODE;
 
 const redirect = encodeURIComponent('http://likely.cloud:50451/api/discord/callback');
 
@@ -55,35 +68,58 @@ router.get('/callback', catchAsync(async (req, res) => {
 
     });
 
-    //console.log(util.inspect(`Bearer ` + token_json, {sowHidden: false, depth: null}));
-
     const token_json = await token_response.json();
 
     console.log(util.inspect(token_json, {sowHidden: false, depth: null}));
 
-    console.log(util.inspect(token_json["access_token"], {sowHidden: false, depth: null}));
+    //console.log(util.inspect(token_json["access_token"], {sowHidden: false, depth: null}));
 
 
     //
-    //  Get the user @todo: this block is failing with method not allowed error
+    //  Get the user
     //
-    const profile_response = await fetch(`https://discordapp.com/api/users/@me`,
+    const user_response = await fetch(`https://discordapp.com/api/users/@me`,
     {
 
         method: 'GET',
 
         headers: {
 
-          Authorization: `Bearer ` + token_json["access_token"],
+          Authorization: `Bearer ` + token_json.access_token,
 
         },
 
     });
 
-    const profile_json = await profile_response.json();
+    const user_json = await user_response.json();
 
-    console.log(util.inspect(profile_json, {sowHidden: false, depth: null}));
+    console.log(util.inspect(user_json, {sowHidden: false, depth: null}));
 
+
+    //
+    //  Get the invite
+    //
+    const invite_response = await fetch(`https://discordapp.com/api/invites/`,
+    {
+      
+        method: 'GET',
+        
+        headers: {
+          
+          Authorization: `Bearer ` + token_json.access_token,
+          
+        },
+        
+    });
+    
+    const invite_json = await invite_response.json();
+
+    console.log(util.inspect(invite_json, {sowHidden: false, depth: null}));
+
+
+    //
+    //  Redirect user to homepage
+    //
     res.redirect(`/?token=${token_json.access_token}`);
 
 }));
