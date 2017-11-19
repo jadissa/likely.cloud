d7a9441abce3c647daa2d485e585d7f959173052
Jadissa Griffin <jadissag@gmail.com>
2017-11-19 06:34:46 -0800

    Added ability to get user's GeoIP info

--- tests/geoip-lite.js
+++ tests/geoip-lite.js
@@ -1,6 +1,27 @@
+var ip;
+
+var http = require('http');
+
 var geoip = require('geoip-lite');
- 
-var ip = '138.197.206.13';
-var geo = geoip.lookup(ip);
- 
-console.log(geo);
+
+http.createServer(function (req, res) {
+
+    if (req.headers['x-forwarded-for']) {
+
+        ip = req.headers['x-forwarded-for'].split(",")[0];
+
+    } else if (req.connection && req.connection.remoteAddress) {
+
+        ip = req.connection.remoteAddress;
+
+    } else {
+
+        ip = req.ip;
+
+    }
+
+    var geo = geoip.lookup(ip);
+
+    console.log(geo);
+    
+}).listen(50451);
