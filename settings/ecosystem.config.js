a2648258bdb3fcf9d151f4f56c3bf7f0f49bfd8f
Jadissa Griffin <jadissag@gmail.com>
2017-12-10 18:48:16 -0800

    Added ignore watch to logs

--- settings/ecosystem.config.js
+++ settings/ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 NODE_ENV: 'production'
             },
             "watch": "./",
-            "ignore_watch" : ["node_modules", "client/img", "likely.log", "out.log", "discord.log"],
+            "ignore_watch" : ["node_modules", "client/img", "likely.log", "logs"],
             "watch_options": {
                 "followSymlinks": false
             },
