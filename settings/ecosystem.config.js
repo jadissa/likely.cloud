35d367762cbe473316fbb95eb17047c68a8a8f0e
jadissa <jadiilelz@gmail.com>
2018-01-02 04:27:11 +0000

    Documented server restart requirement

--- settings/ecosystem.config.js
+++ settings/ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     /**
      * Application configuration section
      * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     *
+     * Changes to this file require a server restart
      */
 
     apps : [
